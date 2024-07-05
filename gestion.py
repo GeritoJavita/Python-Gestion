2728333cef3625baa52ba849e937fc592d484e10
Gerito <you@example.com>
2024-07-04 21:09:22 -0500

    Primer Archivo

--- gestion.py
+++ gestion.py
@@ -1,1 +1,34 @@
-print("Hola mundo")
+
+#La libreria random nos permite gestionar de forma aleatoria entre rangos
+import random
+import statistics
+import math
+import json
+import os
+
+#Lista de productos que nos da el enunciado
+lista_productos = ["televisor", "laptop", "telefono movil", "tablet", "camara digital",
+                   "reproductor de musica", "impresora", "auriculares", "microfono profesional", "robot aspirador"]
+
+
+
+#print(lista_productos)
+
+#Diccionario para almacenar los productos, con sus respectivos precios y cantidades
+inventario = {}
+
+#Funcion para asignar los precios y cantidades aleatorias
+
+
+
+for producto in lista_productos:
+    precio = random.randint(100000, 500000)
+    cantidad = random.randint(1, 100)
+    inventario[producto] = {'precio': precio, 'cantidad': cantidad}
+
+
+os.system("cls")
+print(inventario)
+
+
+
